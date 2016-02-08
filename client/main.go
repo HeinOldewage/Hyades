e4d8ea73170f8f7d7fbebb8569071161e0e1fc9b
HeinOldewage <heinoldewage@gmail.com>
2016-02-08 11:52:43 +0200

    Added mongo DB and basic server connection

--- client/main.go
+++ client/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
- "fmt"
-
+	"fmt"
+	"log"
+	"net"
 )
 
+type TaurusClient struct {
+	ServerAddress string
+	conn          net.Conn
+}
+
+func (tc *TaurusClient) Connect() (err error) {
+	tc.conn, err = net.Dial("tcp", tc.ServerAddress)
+	return err
+}
 
 func main() {
- fmt.Println("This is the taurus client")
+	fmt.Println("This is the taurus client")
+	tc := TaurusClient{ServerAddress: ":8080"}
+	err := tc.Connect()
+	if err != nil {
+		log.Println(err)
+	}
+	select {}
+
 }
--- server/distributionServer/main.go
+++ server/distributionServer/main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-"fmt"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
 )
 
+type TaurusServer struct {
+	ListenAddress string
+	DBhandle      *DB
+}
+
+func (ts *TaurusServer) Listen() error {
+	listener, err := net.Listen("tcp", ts.ListenAddress)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+	for {
+		conn, _ := listener.Accept()
+		log.Println("Got connection")
+		go ts.handle(conn)
+	}
+}
+
+func (ts *TaurusServer) handle(conn net.Conn) {
+	defer conn.Close()
+	job := ts.DBhandle.GetNextJob()
+	log.Println("Got a job", job)
+	data, err := json.Marshal(job)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	conn.Write(data)
+}
+
 func main() {
- fmt.Println("This is the distribution server")
+	fmt.Println("This is the distribution server")
+	db, err := NewDB()
+	if err != nil {
+		log.Println(err)
+	}
+	ts := TaurusServer{ListenAddress: ":8080", DBhandle: db}
+	ts.Listen()
+
 }
