c5e49db2f3ce227696d4dc7fe7d2ad9706b9c8bf
HeinOldewage <heinoldewage@gmail.com>
2016-02-15 10:46:01 +0200

    Zipping folders added

---
+++ ziphelper.go
@@ -0,0 +1,64 @@
+package Hyades
+
+import (
+	"archive/zip"
+	"bytes"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+)
+
+func addtozip(z *zip.Writer, name string, f *os.File) {
+
+	if fi, _ := f.Stat(); fi.IsDir() {
+		log.Println("Adding folder", name)
+		names, _ := f.Readdirnames(-1)
+		for _, subname := range names {
+			file, err := os.Open(filepath.Join(f.Name(), subname))
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			addtozip(z, filepath.Join(name, subname), file)
+			file.Close()
+		}
+	} else {
+		log.Println("Adding file", name)
+		fw, err := z.Create(name)
+		if err != nil {
+			panic(err)
+		}
+		_, err = io.Copy(fw, f)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func ZipCompressFolder(folder string) []byte {
+	envfile := new(bytes.Buffer)
+
+	zipper := zip.NewWriter(envfile)
+
+	scan, err := os.Open(folder)
+	if err != nil {
+		panic(err)
+	}
+	defer scan.Close()
+
+	log.Println("Calling addtozip")
+	names, _ := scan.Readdirnames(-1)
+	for _, subname := range names {
+		file, err := os.Open(filepath.Join(folder, subname))
+		if err != nil {
+			continue
+		}
+		addtozip(zipper, subname, file)
+		file.Close()
+	}
+	//addtozip(zipper,folder,scan)
+
+	zipper.Close()
+	return envfile.Bytes()
+}
